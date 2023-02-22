7bf90b8f4a04edc592801c1ef595d6d417cd6a77
Andres <andresfelipebustosr55@gmail.com>
2023-02-22 11:21:32 -0500

    1st commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const hamburger = document.querySelector(".hamb");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", function() {
+  navLinks.classList.toggle("open");
+
+})
+
+
+
